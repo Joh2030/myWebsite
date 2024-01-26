d56d116bfc55f55097556289582f737dd6f0a01d
Joh2030 <joram291@gmail.com>
2024-01-26 14:02:58 +0100

    additional changes

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+  });
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Timer />);
