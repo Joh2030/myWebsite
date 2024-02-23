a0621686fe1bbbb03052a620c0f3f36e322764e7
Joh2030 <joram291@gmail.com>
2024-02-23 17:02:23 +0100

    added navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div>About</div>
+      <div>Skills</div>
+      <div>Blogs</div>
+      <div>Contact</div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-function Timer() {
-  const [count, setCount] = useState(0);
+// function Timer() {
+//   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  });
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setCount((count) => count + 1);s
+//     }, 1000);
+//   });
 
-  return <h1>I've rendered {count} times!</h1>;
-}
+//   return <h1>I've rendered {count} times!</h1>;
+// }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer />);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Timer />);
