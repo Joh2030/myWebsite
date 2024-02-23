21a958f0cec25fdd074cfb317a9c719b74c1f513
Joh2030 <joram291@gmail.com>
2024-02-23 17:12:00 +0100

    added herosection

---
+++ src/Components/Herosection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Herosection() {
+  return (
+    <div className="Herosection">
+      <div>
+        <h1>Hello, this is Joram Gathundo,</h1>
+        <p>
+          I'm a results-driven Fullstack Web Developer passionate about crafting
+          seamless digital experiences. Explore my projects and let's bring your
+          ideas to life!
+        </p>
+        <h2>SCROLL FOR MORE</h2>
+      </div>
+      <div className="image">
+        <img src="/images/me.jpg" alt="Joram Gathundo" />
+      </div>
+    </div>
+  );
+}
+
+export default Herosection;
