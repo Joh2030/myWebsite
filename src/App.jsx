15523a9023ecd17aff123099cda2c860972c04f3
Joh2030 <joram291@gmail.com>
2024-02-23 19:33:17 +0100

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { Routes, Route, NavLink } from "react";
-import Navbar from "../src/Components/Navbar";
-
 import "./App.css";
 
 export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>{/* <Route path="/" element={<HeroSection />} /> */}</Routes>
-    </>
-  );
+  return <></>;
 }
