7a6369905054ef8375d30b5d77c5b00c70fce5cb
Joh2030 <95764465+Joh2030@users.noreply.github.com>
2024-02-27 19:17:39 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,22 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
 
-function Timer() {
-  const [count, setCount] = useState(0);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setCount((count) => count + 1);
-  //   }, 1000);
-  // });
-
-  return <h1>I've rendered {count} times!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
