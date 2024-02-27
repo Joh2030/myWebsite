c04c2b617824f4283d51ab8dc8c6d0800ebf7460
Joh2030 <joram291@gmail.com>
2024-02-27 20:21:35 +0100

    final2

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Testimonials from "./Components/Testimonials";
-import HeroSection from "./Components/Herosection";
+import Herosection from "./Components/Herosection";
 import Projects from "./Components/Projects";
 import Blogs from "./Components/Blogs";
 import Contact from "./Components/Contact";
-import Herosection from "./Components/Herosection";
 
 export default function App() {
   return (
