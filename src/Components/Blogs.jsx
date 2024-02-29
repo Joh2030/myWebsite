12826ed96592871af2bb5931101910048be685e2
Joh2030 <joram291@gmail.com>
2024-03-01 00:17:18 +0100

    updated the blogs card

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -9,46 +9,41 @@ import Chatgpt from "../assets/chatgpt.jpg";
 export default function Blogs() {
   return (
     <div
-      className="blogs p-5"
-      style={{ background: "#F0EDCF", fontFamily: "Roboto", height: "100%" }}
+      className="blogs pt-4"
+      style={{ background: "#F0EDCF", fontFamily: "Roboto", height: "80vh" }}
     >
       <h1 className="text-center text-3xl font-bold m-5">Blogs</h1>
-      <div className="container-blogs text-center d-flex justify-content-around mt-8">
+
+      <div className="container d-flex justify-content-between flex-wrap">
         <div className="Blog1">
-          <div className="flex justify-between my-4">
-            <BlogsCard
-              img={Hooks}
-              heading="React Hooks Cheatsheet"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ratione
+          <BlogsCard
+            img={Hooks}
+            heading="React Hooks Cheatsheet"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ratione
         dolore in dolor quam veniam tenetur aperiam, deserunt similique sequi,
         magnam ipsa, laborum consequatur itaque asperiores vitae dignissimos.
         Provident, expedit."
-            />
-          </div>
+          />
         </div>
         <div className="Blog1">
-          <div className="justify-between my-4">
-            <BlogsCard
-              img={Chatgpt}
-              heading="How to use ChatGPT in daily Life"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ratione
+          <BlogsCard
+            img={Chatgpt}
+            heading="How to use ChatGPT in daily Life"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ratione
         dolore in dolor quam veniam tenetur aperiam, deserunt similique sequi,
         magnam ipsa, laborum consequatur itaque asperiores vitae dignissimos.
-        Provident, expedit. Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            />
-          </div>
+        Provident, expedit. "
+          />
         </div>
         <div className="Blog1">
-          <div className="justify-between my-4">
-            <BlogsCard
-              img={Kevin}
-              heading="Frontend Optimization Checklist"
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ratione
-        dolore in dolor quam veniam tenetur aperiam, deserunt similique sequi,
-        magnam ipsa, laborum consequatur itaque asperiores vitae dignissimos.
-        Provident, expedit. Lorem ipsum dolor sit amet consectetur adipisicing elit."
-            />
-          </div>
+          <BlogsCard
+            img={Hooks}
+            heading="Frontend Optimization Checklist"
+            text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ratione
+            dolore in dolor quam veniam tenetur aperiam, deserunt similique sequi,
+            magnam ipsa, laborum consequatur itaque asperiores vitae dignissimos.
+            Provident, expedit."
+          />
         </div>
       </div>
     </div>
